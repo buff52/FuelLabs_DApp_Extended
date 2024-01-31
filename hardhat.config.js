092a041ea089920c2ae087bb3a7e187c6eede454
buff52 <152535738+buff52@users.noreply.github.com>
2024-01-31 04:30:13 +0000

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+// hardhat.config.js
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    // Add network configurations as needed (e.g., localhost, Rinkeby, etc.)
+  },
+};
